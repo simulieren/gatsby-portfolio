e165597fa3d73c283858b2004be3738b36ec4a6f
Simon Halimonov <hello@simonhalimonov.de>
2019-04-06 14:36:05 +0200

    Add eslint-config-wesbos

--- src/components/Animations/HeaderSlider.tsx
+++ src/components/Animations/HeaderSlider.tsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import { Spring, Transition, animated, config } from 'react-spring'
-import { render } from 'react-dom'
+import React from "react";
+import { Spring, Transition, animated, config } from "react-spring";
+import { render } from "react-dom";
 
 class HeaderFader extends React.Component {
-  state = { textItems: ['Hello', 'Heya :)', 'WAZZUP'], index: 0, timer: false }
+  state = { textItems: ["Hello", "Heya :)", "WAZZUP"], index: 0, timer: false };
 
-  faderInterval: any = null
+  faderInterval: any = null;
 
   setFaderInterval = () => {
     this.faderInterval = setInterval(() => {
-      console.log(this.state.index)
-      if (!this.state.timer) return
-      let i = this.state.index
-      i++
-      i = i % this.state.textItems.length
-      this.setState({ index: i })
-    }, this.props.time || 2000)
-  }
+      console.log(this.state.index);
+      if (!this.state.timer) return;
+      let i = this.state.index;
+      i++;
+      i = i % this.state.textItems.length;
+      this.setState({ index: i });
+    }, this.props.time || 2000);
+  };
 
   componentDidMount() {
-    this.setState({ textItems: this.props.items, timer: true })
-    this.setFaderInterval()
+    this.setState({ textItems: this.props.items, timer: true });
+    this.setFaderInterval();
   }
 
   componentWillMount() {
-    this.setState({ timer: false })
-    clearInterval(this.faderInterval)
+    this.setState({ timer: false });
+    clearInterval(this.faderInterval);
   }
 
   render() {
     return (
-      <div style={{ perspective: '400px' }}>
+      <div style={{ perspective: "400px" }}>
         {this.state.textItems.map((text, i) => {
           return (
             <Transition
@@ -38,22 +38,31 @@ class HeaderFader extends React.Component {
               keys={item => item}
               // trail={-1000}
               // config={{duration: this.props.duration}}
-              from={{ opacity: 0, transform: 'translateY(-100%) rotateX(60deg)' }}
-              enter={{ opacity: 1, transform: 'translateY(0%) rotateX(0deg)' }}
-              leave={{ opacity: 0, transform: 'translateY(100%) rotateX(-60deg)' }}
+              from={{
+                opacity: 0,
+                transform: "translateY(-100%) rotateX(60deg)"
+              }}
+              enter={{ opacity: 1, transform: "translateY(0%) rotateX(0deg)" }}
+              leave={{
+                opacity: 0,
+                transform: "translateY(100%) rotateX(-60deg)"
+              }}
             >
               {index => props =>
                 index === i && (
-                  <animated.div style={{ ...props, position: 'absolute', width: '100%' }} key={index}>
+                  <animated.div
+                    style={{ ...props, position: "absolute", width: "100%" }}
+                    key={index}
+                  >
                     {text}
                   </animated.div>
                 )}
             </Transition>
-          )
+          );
         })}
       </div>
-    )
+    );
   }
 }
 
-export default HeaderFader
+export default HeaderFader;
