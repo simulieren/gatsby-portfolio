14a78cf92e230b7cb83e291a7610e496be02bc80
Simon <hello@simonhalimonov.de>
2019-10-15 08:46:58 +0700

    Fix: Remove about page

--- data/NavigationLinks.ts
+++ data/NavigationLinks.ts
@@ -2,9 +2,9 @@
 const navLinks = [
   { text: `work`, to: `/work` },
   { text: `service`, to: `/service` },
-  { text: `about`, to: `/about` },
   { text: `hire-me`, to: `/hire-me` },
   { text: `notes`, to: `/notes` },
+  // { text: `about`, to: `/about` },
   // { text: `contact`, to: `/kontakt` },
   // { text: `blog`, to: `/blog` },
   // { text: `notes`, to: `/notizen` }
